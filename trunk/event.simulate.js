e06af6ec644224f7df8fa290bf2915393466f9ab
kangax <kangax@b5f8d413-b845-0410-959f-4dbcb5987c91>
2008-02-13 16:17:50 +0000

    support for HTMLEvents in event.simulate

--- trunk/event.simulate.js
+++ trunk/event.simulate.js
@@ -1,5 +1,6 @@
 Event.simulate = function(element, eventName) {
-  var oEvent, options = Object.extend({
+  
+  var options = Object.extend({
     pointerX: 0,
     pointerY: 0,
     button: 0,
@@ -10,12 +11,30 @@ Event.simulate = function(element, eventName) {
     bubbles: true,
     cancelable: true
   }, arguments[2] || { } );
+  
+  var eventMatchers = {
+    'HTMLEvents': /load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll/,
+    'MouseEvents': /click|mousedown|mouseup|mouseover|mousemove|mouseout/
+  };
+  
+  var oEvent, eventType = null;
+  
+  for (var name in eventMatchers) {
+    if (eventMatchers[name].test(eventName)) eventType = name;
+  }
+  
+  if (!eventType) throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
 
   if (document.createEvent) {
-    oEvent = document.createEvent("MouseEvents");
-    oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView, 
-      options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
-      options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, $(element));
+    oEvent = document.createEvent(eventType);
+    if (eventType == 'HTMLEvents') {
+      oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+    }
+    else {
+      oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView, 
+        options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+        options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, $(element));
+    }
     $(element).dispatchEvent(oEvent);
   }
   else {
