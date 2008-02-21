9c954dacf4cd453688aaeb4d6b046f55a2380edf
kangax <kangax@b5f8d413-b845-0410-959f-4dbcb5987c91>
2008-02-21 16:43:39 +0000

    key observer now works with tab

--- trunk/key_observer.js
+++ trunk/key_observer.js
@@ -5,20 +5,19 @@ Example:
 document.observe('key:esc', helpWindow.close);
 document.observe('key:space', focusNextField);
 
-Notes:
-tab event is NOT working (apparently due to keyup)
-
 */
  
+
 (function() {
   var hash = { 32: 'space', 19: 'pause' };
+  var handler = function(e) {
+    if (!hash[e.keyCode]) return;
+    if (e.type == 'keydown' && e.keyCode != Event.KEY_TAB) return;
+    document.fire('key:' + hash[e.keyCode], e);
+  }
   for (var prop in Event) {
     if (prop.startsWith('KEY_'))
       hash[Event[prop]] = prop.replace('KEY_', '').toLowerCase();
   }
-  document.observe('keyup', function(e) {
-    if (!hash[e.keyCode]) return;
-    document.fire('key:' + hash[e.keyCode], e);
-  })
+  document.observe('keyup', handler).observe('keydown', handler);
 })();
-
