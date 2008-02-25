d6aa906299d3aa3b2c91ccee16a9898a6196b99c
kangax <kangax@b5f8d413-b845-0410-959f-4dbcb5987c91>
2008-02-25 17:20:17 +0000

    Enhance Field#present

--- src/helpers.js
+++ src/helpers.js
@@ -159,7 +159,30 @@ document.observe('click', function(e) {
   var input = $(target.readAttribute('for'));
   if (!input) return;
   input.focus();
-  if (!Object.isUndefined(input.checked)) {
-    input.checked = !input.checked;
+  if (input.type && (/radio|checkbox/).test(input.type)) {
+    input.click();
   }
 })
+
+
+/**
+ * Boosts Field#present to work somewhat more reasonably 
+ * with any form control (element with type attribute)
+ * 
+ * - "reset, submit and button" always return true
+ * - "text, password and file" return true if value attribute is anything but whitespace
+ * - "checkbox" return true if checked attribute is not false
+ * - "radio" return true if either checked attribute is not false 
+ *   or any of the input elements with the same name have checked attribute and it's not false
+ * - "select" return true if selectedIndex attribute is not -1
+ *
+ */
+Field.Methods.present = function(element) {
+  if (!(element = $(element)) && !element.type) return;
+  var t = element.type;
+  return ((/text|password|file/.test(t) && !element.value.blank()) ||
+    /reset|submit|button/.test(t) ||
+    (t == 'checkbox' && element.checked) ||
+    (t == 'radio' && (element.checked || $$('input[name=' + element.name + ']:checked').length))
+    (/select-one|select-multiple/.test(t) && element.selectedIndex != -1));
+}
